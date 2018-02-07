7d05926076be6836e7d3247e0d8fde83dae53716
RaetzkeMat <matthias.raetzke@zueblin.de>
2018-02-07 20:46:02 +0100

    Cat Clicker Premium Pro

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ var model = {
             name : 'Sleeping Cats',
             imgSrc : 'img/cat3.jpg',
         }
-    ]
+	],
+	adminVisible : false
 };
 
 var octopus = {
@@ -24,21 +25,49 @@ var octopus = {
 		model.currentCat = model.cats[0];
 		CatListView.init();
 		CatView.init();
-		document.getElementById('cat-img').addEventListener('click', function(){
-			model.currentCat.clickCount++;
-			CatView.render();
-		});
-	}	
+		ButtonView.init();
+		FormView.init();
+	},
+	getCurrentCat: function (){
+		return model.currentCat;
+	},
+	setCurrentCat: function (obj){
+		model.currentCat = obj;
+	},
+	getAllCats: function () {
+		return model.cats;
+	},
+	incrementCats: function(){
+		model.currentCat.clickCount++;
+	},
+	getClickCount: function(){
+		return model.currentCat.clickCount;
+	},
+	changeCat: function(obj){
+		model.currentCat.name = obj.name;
+		model.currentCat.imgSrc = obj.imgSrc;
+		model.currentCat.clickCount = obj.clickCount;
+	},
+	isAdmin: function(){
+		return model.adminVisible;
+	},
+	setAdmin(){
+		model.adminVisible = ! model.adminVisible;
+	}
 }
 
 
  var CatListView = {
-	init: function(){		
+	init: function(){
+		
 		// makes new object into this object 
 		this.catListElem = document.getElementById('cat-list');		
+		this.catListElem.innerHTML = "";
 		var cat;
-		 for(i = 0; i < model.cats.length; i++){		
-			cat = model.cats[i];		 
+		
+		 for(i = 0; i < octopus.getAllCats().length; i++){		
+			cat = octopus.getAllCats()[i];
+			
 			var li = document.createElement('li');
 			li.innerHTML = li.innerHTML + cat.name;
 			this.catListElem.appendChild(li);
@@ -46,9 +75,10 @@ var octopus = {
 			
 			li.addEventListener('click', (function(catCopy) {
                 return function() {
-					document.getElementById('cat-img').src = catCopy.imgSrc;
-					model.currentCat = catCopy;
+					//document.getElementById('cat-img').src = catCopy.imgSrc;
+					octopus.setCurrentCat(catCopy);
 					CatView.render();
+					FormView.render();
                 };
             })(cat));
 		}
@@ -56,11 +86,71 @@ var octopus = {
  }
 
  var CatView = {
-	init: function(){		
+	init: function(){	
+		document.getElementById('cat-img').addEventListener('click', function(){
+			octopus.incrementCats();
+			CatView.render();
+			FormView.render();
+		});
+		this.render();
+	},
+	render: function(){		
+		document.getElementById('cat-counter').innerHTML = octopus.getClickCount();
+		document.getElementById('cat-img').src = octopus.getCurrentCat().imgSrc;		
+	}
+}
+
+var ButtonView = {
+	init: function() {
+		this.adminbutton = document.getElementById('admin-button');
+		this.adminbutton.addEventListener('click', function() {
+			octopus.setAdmin();
+			FormView.render();
+		}
+		);
+		//this.render();
+	},
+	render: function(){
+		// not needed!!!
+		
+	}
+}
+
+var FormView = {
+	init: function() {
+		this.adminform = document.getElementById('admin-form');
+		octopus.inputName = document.getElementById('inputName');
+		octopus.inputImgUrl = document.getElementById('inputImgUrl');
+		octopus.inputclicks = document.getElementById('inputclicks');
+		
+		
+		this.okButton = document.getElementById('ok-button');
+		this.okButton.addEventListener('click', function() {
+			octopus.getCurrentCat().name = octopus.inputName.value;
+			octopus.getCurrentCat().imgSrc = octopus.inputImgUrl.value;
+			octopus.getCurrentCat().clickCount = octopus.inputclicks.value;
+			octopus.setAdmin();
+			CatListView.init();
+			CatView.render();
+			FormView.render();
+		});
+
+		this.cancelButton = document.getElementById('cancel-button');
+		this.cancelButton.addEventListener('click', function(){			
+			octopus.setAdmin();
+			FormView.render();
+		});
 		this.render();
 	},
 	render: function(){
-		document.getElementById('cat-counter').innerHTML = model.currentCat.clickCount;
+		octopus.inputName.value = octopus.getCurrentCat().name;
+		octopus.inputImgUrl.value = octopus.getCurrentCat().imgSrc;
+		octopus.inputclicks.value = octopus.getCurrentCat().clickCount;
+		if(octopus.isAdmin()){
+			this.adminform.style.visibility = "visible";			
+		}else{
+			this.adminform.style.visibility = "hidden";
+		}
 	}
 }
 
